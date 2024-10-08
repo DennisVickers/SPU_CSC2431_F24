f92d93f9e701d99994ffc9768ca2885740b85776
Prof Vickers <dennis.vickers@outlook.com>
2024-10-08 10:24:33 -0700

    update

--- Module 1/BubbleSort.cpp
+++ Module 1/BubbleSort.cpp
@@ -2,57 +2,57 @@
 #include <vector>
 using namespace std;
 
-// Bubble Sort Algorithm
-void BubbleSort(vector<int> &list) {
-   
-   bool swapped;
-   int lastElement;
-   int lastUnsorted;
+void BubbleSort(vector<int> &list);
+void printList(const vector<int> &list);
 
-   // Outer loop
-   lastElement = (list.size() - 1);
-   lastUnsorted = lastElement;
-   for (int i = 0; i < lastElement; i++) {
-      swapped = false;
-      // Inner Loop
-      for (int j = 0; j < lastUnsorted; j++) {
-         if (list.at(j) > list.at(j + 1)) {
-            // Swap elements
-            int temp = list.at(j);
-            list.at(j) = list.at(j + 1);
-            list.at(j + 1) = temp;
-            swapped = true;
-         }
-      }
-      // Reduce the search search space
-      lastUnsorted--;
-      
-      // If no two adjacent elements were swapped in inner loop, the listay is sorted
-      if (!swapped) {
-         break;
-      }
-   }
-}
-
-// Function to print the listay
-void printList(const vector<int> &list) {
-    for (int num : list) {
-        cout << num << " ";
-    }
-    cout << endl;
-}
-
-// Main function to test the bubble sort
 int main() {
-    vector<int> list = {42, 37, 12, 3, 9};
+  vector<int> list = {42,37,12,3,9};
 
-    cout << "Original list: ";
-    printList(list);
+  cout << "Unsorted list: ";
+  printList(list);
 
-    BubbleSort(list);
+  BubbleSort(list);
 
-    cout << "Sorted list: ";
-    printList(list);
+  cout << "Sorted list: ";
+  printList(list);
 
-    return 0;
+  return 0;
+}
+
+void printList(const vector<int> &list) {
+  for (int num : list) {
+    cout << num << " ";
+  }
+  cout << endl;
+}
+
+void BubbleSort(vector<int> &list) {
+  bool swapped;
+  int lastElement;
+  int lastUnsorted;
+
+  // Outer loop
+  lastElement = (list.size() - 1);
+  lastUnsorted = lastElement;
+  for (int i=0; i < lastElement; i++) {
+    swapped = false;
+    // inner loop
+    for (int j=0; j< lastUnsorted; j++) {
+      if (list.at(j) > list.at(j + 1)) {
+        // swap elements
+        int temp = list.at(j);
+        list.at(j) = list.at(j+1);
+        list.at(j+1) = temp;
+        swapped = true;
+      }
+    }
+    // reduce the search space
+    lastUnsorted--;
+
+    // if no two adjacent elements were swapped in the
+    // inner loop the list is sorted
+    if (!swapped) {
+      break;
+    }
+  }
 }
